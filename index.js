24c312c40e81b74b3dcb55fec88c96ee1da7bdb3
Bryan Cuevas <bryancuevas4@gmail.com>
2019-04-19 21:50:34 -0700

    Removed commented code

--- index.js
+++ index.js
@@ -19,34 +19,3 @@ bot.on('message', (msg) => {
 });
 
 bot.login(DISCORD_TOKEN);
-// const Discord = require('discord.js');
-// const client = new Discord.Client();
-// const OverwatchStats = require('./overwatchStats');
-
-// require('dotenv').config()
-
-// const botPrefix = '!';
-// const DISCORD_TOKEN = process.env.BOT_TOKEN;
-// const owStats = new OverwatchStats('pc', 'us', 'Slim-1958');
-// // client.on('message', msg => {
-// //     if(msg.content == "ping") {
-// //         msg.reply('pong');
-// //     }
-// // });
-
-// // client.on('message', msg => {
-// //     if(!msg.content.startsWith(prefix) || msg.author.bot) return;
-
-// //     const args = msg.content.slice(prefix.length).split(' ');
-// //     const command = args.shift().toLowerCase();
-
-// //     msg.reply(`Command: ${command}`);
-// // });
-
-// client.on('message', async msg => {
-//     if(!msg.content.startsWith(botPrefix) || msg.author.bot) return;
-//     let playerData = await owStats.getStats();
-//     msg.reply(`Competitive Games won for ${playerData.data.name}: ${playerData.data.competitiveStats.games.gamesWon}`);
-// });
-
-// client.login(DISCORD_TOKEN);
